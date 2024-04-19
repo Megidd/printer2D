746ea4cb7773f7749b1c66c415f789f099a99d73
Megidd <megiddgit@gmail.com>
2024-04-19 01:20:52 -0700

    Comment

--- utils/send.py
+++ utils/send.py
@@ -4,6 +4,7 @@ import subprocess
 
 def send2printer(pdfPath):
     # https://mendelson.org/pdftoprinter.html
+    # https://stackoverflow.com/a/47994723/3405291
     toPrinterPath = shutil.which("PDFtoPrinter.exe")
     if toPrinterPath is not None:
         subprocess.run(f'PDFtoPrinter.exe "{pdfPath}"', shell=True)
