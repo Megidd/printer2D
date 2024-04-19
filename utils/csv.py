9cb691d66761dcf19a5116f13bff3aa41d0ebec2
Megidd <megiddgit@gmail.com>
2024-04-19 06:31:54 -0700

    Consider the count

--- utils/csv.py
+++ utils/csv.py
@@ -17,17 +17,25 @@ def find_and_delete_row(filename, varl_value=600, varw_value=700):
 
     found = False
     for row in reader:
-      if not found and row[6] == str(varl_value) and row[8] == str(varw_value) or \
-        not found and row[6] == str(varw_value) and row[8] == str(varl_value):
+      count = row[5]
+      if count.isdigit():
+        count = int(count)
+      else:
+        print("Warning: count string is not a valid integer.")
+        continue
+      if not found and count > 0 and row[7] == str(varl_value) and row[9] == str(varw_value) or \
+        not found and count > 0 and row[7] == str(varw_value) and row[9] == str(varl_value):
         id, name = row[3], row[4]
         project, floor, unit = row[0], row[1], row[2]
         found = True
-        print(f"Found line: {id}, {name}")
+        row[5] = count -1
+        print(f"Found line: {id}, {name}, count change: {count} > {row[5]}")
+        writer.writerow(row)
       else:
         writer.writerow(row)
 
     if not found:
-      print(f"Line with VARL: {varl_value} and VARW: {varw_value} is already deleted from the CSV file.")
+      print(f"Line with VARL: {varl_value} and VARW: {varw_value} is already handled on the CSV file.")
 
   move_and_overwrite(tmpFile, filename)
   return project, floor, unit, id, name
