c999924fac229401c22e43f3118e7cc8ecc4ad58
Megidd <megiddgit@gmail.com>
2024-04-19 00:42:49 -0700

    Try this: https://mendelson.org/pdftoprinter.html

--- utils/send.py
+++ utils/send.py
@@ -3,8 +3,8 @@ import shutil
 import subprocess
 
 def send2printer(pdfPath):
-    AcrobatReaderPath = shutil.which("AcroRd32.exe")
-    if AcrobatReaderPath is not None:
-        subprocess.run(f'AcroRd32.exe /t "{pdfPath}"', shell=True)
+    toPrinterPath = shutil.which("PDFtoPrinter.exe")
+    if toPrinterPath is not None:
+        subprocess.run(f'PDFtoPrinter.exe "{pdfPath}"', shell=True)
     else:
         os.startfile(pdfPath, "print")
