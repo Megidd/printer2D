9576bd92b226901f7bb372864187c363c4586b61
Megidd <megiddgit@gmail.com>
2025-02-12 10:17:55 -0500

    Rename files

--- run_barcode_scanner.cmd
+++ run_barcode_scanner.cmd
@@ -1,6 +1,6 @@
 python -m venv virtual_env & ^
 virtual_env\Scripts\activate.bat & ^
 python -m pip install -r requirements.txt & ^
-python script_barcode-scanner.py & ^
+python script_barcode_scanner.py & ^
 deactivate & ^
 pause
