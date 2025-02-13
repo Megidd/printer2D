3047f9c393c2517262f3e51c73d0a5be39dfd841
Megidd <megiddgit@gmail.com>
2025-02-13 08:37:33 -0500

    Display output CSV temp file

--- script_barcode_scanner.py
+++ script_barcode_scanner.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import filedialog
 import tempfile
+import os
 
 from barcode_scanner.process import compare
 
@@ -13,21 +14,18 @@ def main():
         input_csv_file_entry.delete(0, tk.END)
         input_csv_file_entry.insert(0, filename)
 
-    def browse_output_csv_file():
-        filename = filedialog.asksaveasfilename(title="Select Output CSV File", defaultextension=".csv", filetypes=[("CSV Files", "*.csv")])
-        output_csv_file_entry.delete(0, tk.END)
-        output_csv_file_entry.insert(0, filename)
-
     def run():
         input_barcode_txt = input_barcode_txt_text_box.get("1.0", tk.END)
         input_csv_file = input_csv_file_entry.get()
-        output_csv_file = output_csv_file_entry.get()
 
-        with tempfile.NamedTemporaryFile(mode='w', delete=False) as tmp_file:
-            tmp_file.write(input_barcode_txt)
-            input_barcode_txt_file = tmp_file.name
+        with tempfile.NamedTemporaryFile(mode='w', delete=False) as tmp_input_file:
+            tmp_input_file.write(input_barcode_txt)
+            input_barcode_txt_file = tmp_input_file.name
 
-        compare(input_barcode_txt_file, input_csv_file, output_csv_file)
+        with tempfile.NamedTemporaryFile(mode='w', delete=False, suffix='.csv') as tmp_output_file:
+            output_csv_file = tmp_output_file.name
+            compare(input_barcode_txt_file, input_csv_file, output_csv_file)
+            os.startfile(output_csv_file)
 
     input_barcode_txt_label = tk.Label(root, text="Input Barcode Text:")
     input_barcode_txt_label.grid(row=0, column=0, padx=5, pady=5)
@@ -44,17 +42,8 @@ def main():
     input_csv_file_browse_button = tk.Button(root, text="Browse", command=browse_input_csv_file)
     input_csv_file_browse_button.grid(row=1, column=2, padx=5, pady=5)
 
-    output_csv_file_label = tk.Label(root, text="Output CSV File:")
-    output_csv_file_label.grid(row=2, column=0, padx=5, pady=5)
-
-    output_csv_file_entry = tk.Entry(root, width=50)
-    output_csv_file_entry.grid(row=2, column=1, padx=5, pady=5)
-
-    output_csv_file_browse_button = tk.Button(root, text="Browse", command=browse_output_csv_file)
-    output_csv_file_browse_button.grid(row=2, column=2, padx=5, pady=5)
-
     run_button = tk.Button(root, text="RUN", command=run)
-    run_button.grid(row=3, column=1, padx=5, pady=5)
+    run_button.grid(row=2, column=1, padx=5, pady=5)
 
     root.mainloop()
 
