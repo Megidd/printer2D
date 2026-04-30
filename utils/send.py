1f8d9d40eda173ee73d825ac40debe448aba40f8
Megidd <megiddgit@gmail.com>
2026-04-30 14:56:06 -0400

    Revert "Fix DOS-style short names for user name"
    This reverts commit 15cef4c91435fde7ef280d0bccb18b2ee58d6b62.

--- utils/send.py
+++ utils/send.py
@@ -7,11 +7,7 @@ def send2printer(pdfPath):
     # https://stackoverflow.com/a/47994723/3405291
     toPrinterPath = shutil.which("PDFtoPrinter.exe")
     if toPrinterPath is not None:
-        # Fix the problem:
-        # When pdfPath contains the DOS-style naming and short names
-        # like C:\Users\ADINCU~1\..., the ~1 is being interpreted as a shell metacharacter
-        # subprocess.run(f'PDFtoPrinter.exe "{pdfPath}"', shell=True)
-        subprocess.run(f'"{toPrinterPath}" "{pdfPath}"', shell=True)
+        subprocess.run(f'PDFtoPrinter.exe "{pdfPath}"', shell=True)
     else:
         print(f'Missing "PDFtoPrinter.exe":')
         print("https://github.com/emendelson/pdftoprinter/blob/main/PDFtoPrinter.exe?raw=true")
